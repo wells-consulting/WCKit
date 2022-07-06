57dba41817441b923b76ec98636fd059c9dca462
Michael Wells <michael@michaelwells.com>
2022-07-06 11:26:45 -0700

    Initial commit

---
+++ WCKit/WCKit.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for WCKit.
+FOUNDATION_EXPORT double WCKitVersionNumber;
+
+//! Project version string for WCKit.
+FOUNDATION_EXPORT const unsigned char WCKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WCKit/PublicHeader.h>
